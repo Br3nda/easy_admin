0305c1bb6499c24aac89a8981fbd367bf93f4fe0
Naiki Pohe <naiki.pohe@abletech.co.nz>
2013-01-07 11:00:00 +1300

    rake task to delete old outbound requests

--- lib/easy_admin/engine.rb
+++ lib/easy_admin/engine.rb
@@ -2,4 +2,17 @@ module EasyAdmin
   class Engine < ::Rails::Engine
     require 'will_paginate'
   end
+
+  class Railtie < ::Rails::Railtie
+    rake_tasks do
+      namespace :requests do
+        desc "Delete old outbound requests (DEFAULT= 2.days)."
+        task :clean, [:age] => :environment do |task, params|
+          params.with_defaults(age: 2.days)
+          outbound_request_count = OutboundRequest.delete_requests_older_than(params[:age])
+          puts "Deleted #{outbound_request_count} old Outbound Requests"
+        end
+      end
+    end
+  end
 end
