277cebc365919f4b399041cc7f49733e27fa0b6c
Naiki Pohe <naiki.pohe@abletech.co.nz>
2013-01-07 11:32:40 +1300

    modify rake task to take number (age in days)

--- lib/easy_admin/engine.rb
+++ lib/easy_admin/engine.rb
@@ -6,10 +6,10 @@ module EasyAdmin
   class Railtie < ::Rails::Railtie
     rake_tasks do
       namespace :requests do
-        desc "Delete old outbound requests (DEFAULT= 2.days)."
+        desc "Delete old outbound requests, DEFAULT=2 (days)."
         task :clean, [:age] => :environment do |task, params|
-          params.with_defaults(age: 2.days)
-          outbound_request_count = OutboundRequest.delete_requests_older_than(params[:age])
+          params.with_defaults(age: 2)
+          outbound_request_count = OutboundRequest.delete_requests_older_than(params[:age].to_i.days)
           puts "Deleted #{outbound_request_count} old Outbound Requests"
         end
       end
